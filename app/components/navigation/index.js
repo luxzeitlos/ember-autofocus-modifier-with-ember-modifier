e920efc6f2059f4dbb1ad9af813543795f3026f9
Lux <lux@lux.name>
2023-02-24 17:31:02 +0100

    reproduction

---
+++ app/components/navigation/index.js
@@ -0,0 +1,6 @@
+import Component from '@glimmer/component';
+import autofocus from 'ember-autofocus-modifier/modifiers/autofocus';
+
+export default class Navigation extends Component {
+
+}
---
+++ app/routes/application.js
@@ -0,0 +1,6 @@
+import Route from '@ember/routing/route';
+import Navigation from 'ember-autofocus-modifier-with-ember-modifier/components/navigation';
+import { on } from '@ember/modifier';
+
+export default class ApplicationRoute extends Route {}
+
